4bcbfe87404b18d8a407162e41d79d07e2ef92b2
Anthler <bullanthla@gmail.com>
2019-08-17 04:04:00 -0400

    Updated routes

--- client/index.js
+++ client/index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const router = express.Router();
 
-app.use(express.static(path.join(__dirname, "dist")));
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "dist/index.html"));
+});
+
+//app.use("/", express.static(path.join(__dirname, "dist")));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
