15536dd8cbd0e4e578eb82438bf5453099ef042e
Anthler <bullanthla@gmail.com>
2019-08-17 04:15:14 -0400

    Updated home router

--- client/index.js
+++ client/index.js
@@ -4,11 +4,11 @@ const path = require("path");
 const app = express();
 const router = express.Router();
 
-router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "dist/index.html"));
-});
+// router.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname + "dist/index.html"));
+// });
 
-//app.use("/", express.static(path.join(__dirname, "dist")));
+app.use("/", express.static(path.join(__dirname, "dist")));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
